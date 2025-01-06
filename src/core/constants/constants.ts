ac6b8033ec5ab50b7c22797f2781dcbcca64c5fa
dextermd <dextermdpay@gmail.com>
2025-01-06 20:15:42 +0200

    Small Fix build path

--- src/core/constants/constants.ts
+++ src/core/constants/constants.ts
@@ -1,5 +1,5 @@
 // const apiUrl = 'http://192.168.18.127:3000'; // Home
-const apiUrl = 'http://localhost:3000'; // Production
+const apiUrl = 'https://petstyle.dextermd.dev'; // Production
 
 export const ApiEndpoint = {
   ApiUrl: apiUrl,
